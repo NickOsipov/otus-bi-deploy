d4d8b0c3049145ba6d5810c177d81259f004d845
NickOsipov <nick.osipov.91@gmail.com>
2026-02-19 07:48:52 +0300

    feat(redash): add queries for payment type count and hourly trip count

--- infra/redash/query.sql
+++ infra/redash/query.sql
@@ -1,3 +1,8 @@
-SELECT COUNT(*)
+SELECT COUNT(*) as cnt, payment_type
 FROM default.trips
-GROUP BY payment_type;
+GROUP BY payment_type
+
+SELECT COUNT(trip_id) as cnt, HOUR(pickup_datetime) AS hour
+FROM default.trips
+GROUP BY hour
+ORDER BY hour
